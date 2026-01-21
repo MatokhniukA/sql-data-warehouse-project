07e18c79730c746a580394a26207189f35d85fde
Anastasiia Matokhniuk <matokhniuk.ao@gmail.com>
2026-01-21 13:05:10 +0200

    checked quality of first 2 bronze tables

---
+++ tests/quality_checks_silver.sql
@@ -0,0 +1,80 @@
+USE data_warehouse;
+GO
+
+-- CLEAN & LOAD crm_cust_info
+
+-- Check for Null or Duplicates in Primary Key
+-- Expectation: No Results
+
+SELECT cst_id, COUNT(*) AS cnt
+FROM bronze.crm_cust_info
+GROUP BY cst_id
+HAVING COUNT(*) > 1 OR cst_id IS NULL;
+
+-- Check for Unwanted Spaces in String Values
+-- Expectation: No Results
+
+SELECT cst_firstname AS cst_firstname
+FROM bronze.crm_cust_info
+WHERE cst_firstname != TRIM(cst_firstname);
+
+SELECT cst_lastname AS cst_lastname
+FROM bronze.crm_cust_info
+WHERE cst_lastname != TRIM(cst_lastname);
+
+SELECT cst_marital_status AS cst_marital_status
+FROM bronze.crm_cust_info
+WHERE cst_marital_status != TRIM(cst_marital_status);
+
+SELECT cst_gndr AS cst_gndr
+FROM bronze.crm_cust_info
+WHERE cst_gndr != TRIM(cst_gndr);
+
+-- Data Standardization & Consistency
+
+SELECT DISTINCT cst_gndr
+FROM bronze.crm_cust_info;
+
+SELECT DISTINCT cst_marital_status
+FROM bronze.crm_cust_info;
+
+
+-- CLEAN & LOAD crm_prd_info
+
+-- Check for Null or Duplicates in Primary Key
+-- Expectation: No Results
+
+SELECT prd_id, COUNT(*) AS cnt
+FROM bronze.crm_prd_info
+GROUP BY prd_id
+HAVING COUNT(*) > 1 OR prd_id IS NULL;
+
+-- Check for Unwanted Spaces in String Values
+-- Expectation: No Results
+
+SELECT prd_nm
+FROM bronze.crm_prd_info
+WHERE prd_nm != TRIM(prd_nm);
+
+-- Check for NULLs or Negative Values
+-- Expectation: No Results
+
+SELECT prd_cost
+FROM bronze.crm_prd_info
+WHERE prd_cost < 0 OR prd_cost IS NULL;
+
+-- Data Standardization & Consistency
+
+SELECT DISTINCT prd_line
+FROM bronze.crm_prd_info;
+
+-- Check for Invalid Date Orders
+-- Expectation: No Results
+
+SELECT *
+FROM bronze.crm_prd_info
+WHERE prd_start_dt > prd_end_dt;
+
+
+-- CLEAN & LOAD 
+

f9b82476806927a4a8731adbb0b61d8d7cd08f43
Anastasiia Matokhniuk <matokhniuk.ao@gmail.com>
2026-02-04 13:21:26 +0200

    updated quality_checks_gold

--- tests/quality_checks_gold.sql
+++ tests/quality_checks_gold.sql
@@ -1,13 +1,41 @@
-USE data_warehouse;
+/*
+===============================================================================
+Quality Checks
+===============================================================================
+Script Purpose:
+    This script performs quality checks to validate the integrity, consistency, 
+    and accuracy of the Gold Layer. These checks ensure:
+    - Uniqueness of surrogate keys in dimension tables.
+    - Referential integrity between fact and dimension tables.
+    - Validation of relationships in the data model for analytical purposes.
 
--- Dimension Customers
+Usage Notes:
+    - Investigate and resolve any discrepancies found during the checks.
+===============================================================================
+*/
+
+USE data_warehouse;
+GO
+
+-- ====================================================================
+-- Checking 'gold.dim_customers'
+-- ====================================================================
+
+-- Check for Uniqueness of Customer Key in gold.dim_customers
+-- Expectation: No results 
+SELECT
+    customer_key,
+    COUNT(*) AS duplicate_count
+FROM gold.dim_customers
+GROUP BY customer_key
+HAVING COUNT(*) > 1;
 
 -- Check for Duplicates after Joining Tables
 -- Expectation: No Results
 SELECT cst_id,
-    COUNT(*) AS count
+    COUNT(*) AS duplicate_count
 FROM (
-SELECT ci.cst_id,
+        SELECT ci.cst_id,
         ci.cst_key,
         ci.cst_firstname,
         ci.cst_lastname,
@@ -50,7 +78,18 @@ SELECT DISTINCT gender
 FROM gold.dim_customers;
 
 
--- Dimension Products
+-- ====================================================================
+-- Checking 'gold.dim_products'
+-- ====================================================================
+
+-- Check for Uniqueness of Product Key in gold.dim_products
+-- Expectation: No results 
+SELECT
+    product_key,
+    COUNT(*) AS duplicate_count
+FROM gold.dim_products
+GROUP BY product_key
+HAVING COUNT(*) > 1;
 
 -- Check for Duplicates after Joining Tables
 -- Expectation: No Results
@@ -82,23 +121,16 @@ SELECT *
 FROM gold.dim_products;
 
 
--- Fact Sales
+-- ====================================================================
+-- Checking 'gold.fact_sales'
+-- ====================================================================
 
--- Check Quality of Sales View
-SELECT *
-FROM gold.fact_sales;
-
--- Foreign Key Integrity (Dimensions)
+-- Check the data model connectivity between fact and dimensions (Foreign Key Integrity)
 -- Expectation: No Results
-
 SELECT *
 FROM gold.fact_sales AS s
     LEFT JOIN gold.dim_customers AS c
     ON s.customer_key = c.customer_key
-WHERE c.customer_key IS NULL;
-
-SELECT *
-FROM gold.fact_sales AS s
     LEFT JOIN gold.dim_products AS p
     ON s.product_key = p.product_key
-WHERE p.product_key IS NULL;
+WHERE c.customer_key IS NULL OR p.product_key IS NULL;
